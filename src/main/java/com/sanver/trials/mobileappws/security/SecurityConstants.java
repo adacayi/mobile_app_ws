72c465ac9966d465d73ab8f3ed47ec68faa73d78
Abdullah Sanver <a_sanver@hotmail.com>
2018-10-31 15:23:24 +0000

    AppProperties added to read token secret from application.properties file. Rest service made stateless;

--- src/main/java/com/sanver/trials/mobileappws/security/SecurityConstants.java
+++ src/main/java/com/sanver/trials/mobileappws/security/SecurityConstants.java
@@ -1,10 +1,17 @@
 package com.sanver.trials.mobileappws.security;
 
+import com.sanver.trials.mobileappws.SpringApplicationContext;
+
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME=864_000_000; // 10 days value in milliseconds
-    public static final String TOKEN_PREFIX="bearer ";
-    public static final String HEADER_STRING_TOKEN ="Authorization";
+    public static final long EXPIRATION_TIME = 864_000_000; // 10 days value in milliseconds
+    public static final String TOKEN_PREFIX = "bearer ";
+    public static final String HEADER_STRING_TOKEN = "Authorization";
     public static final String SIGN_UP_URL = "/users/login";
-    public static final String TOKEN_SECRET = "kd832lc3d34ka";
-    public static final String HEADER_STRING_USER_ID="UserID";
+    public static final String TOKEN_SECRET_PROPERTY_NAME = "tokenSecret";
+    public static final String HEADER_STRING_USER_ID = "UserID";
+
+    public static String getTokenSecret() {
+        AppProperties appProperties = SpringApplicationContext.getBean(AppProperties.class);
+        return appProperties.getTokenSecret();
+    }
 }
